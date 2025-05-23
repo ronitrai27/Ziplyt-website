e5da8bf39b77522c3825081e419907c14c04e1e5
ronitrai27 <108011380+ronitrai27@users.noreply.github.com>
2025-05-23 16:28:35 +0530

    final

--- src/components/Wavy.tsx
+++ src/components/Wavy.tsx
@@ -1,25 +1,22 @@
 import Image from "next/image";
 import React from "react";
-
+import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
 const Wavy = () => {
   return (
     <div className="bg-primary-blue w-full h-[10rem] min-[750px]:mt-[15rem] max-[749px]:mt-[12rem] max-[500px]:mt-[6rem] relative mb-10">
       {/* Center image */}
-      <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
-        <Image src="/effectPic.png" alt="image" width={500} height={500} />
+      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 z-50">
+        <Image
+          src="/effectPic.png"
+          alt="image"
+          width={500}
+          height={500}
+          className=""
+        />
       </div>
 
-      {/* Text Content */}
-      <div className="max-[500px]:hidden flex justify-between items-center h-full px-6 min-[800px]:px-20 text-white font-poppins text-[16px] font-medium tracking-tight">
-        {/* Left Text */}
-        <p className="w-[30%] min-w-[140px] text-left leading-tight ">
-          Empowering homes with instant expert help.
-        </p>
-
-        {/* Right Text */}
-        <p className="w-[30%] min-w-[140px] text-right leading-tight">
-          Ziplyt connects you with professionals.
-        </p>
+      <div className="h-full flex items-center justify-center ">
+        <VelocityScroll>FAST.TRANSPARENT.TRUST</VelocityScroll>
       </div>
     </div>
   );
