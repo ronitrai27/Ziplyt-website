57c794ad3752b64c5f0f8faa2cadc01a06176f7c
ronitrai27 <108011380+ronitrai27@users.noreply.github.com>
2025-06-18 20:24:21 +0530

    social icons and images.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,72 +1,75 @@
-import type { Metadata } from 'next';
-import { Inter, Roboto, Open_Sans, Poppins, Quicksand } from 'next/font/google';
-import './globals.css';
-import Footer from '@/components/Footer';
-import { GoogleAnalytics } from '@next/third-parties/google';
-import { Toaster } from 'sonner';
+import type { Metadata } from "next";
+import { Inter, Roboto, Open_Sans, Poppins, Quicksand } from "next/font/google";
+import "./globals.css";
+import Footer from "@/components/Footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { Toaster } from "sonner";
 // import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // Configure fonts
 const openSans = Open_Sans({
-  variable: '--font-main',
-  subsets: ['latin'],
-  weight: ['400', '500', '700'],
+  variable: "--font-main",
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
 });
 
 const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  weight: ['400', '500', '700'],
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
 });
 
 const roboto = Roboto({
-  variable: '--font-roboto',
-  subsets: ['latin'],
-  weight: ['400', '500', '700'],
+  variable: "--font-roboto",
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
 });
 
 const poppins = Poppins({
-  variable: '--font-poppins',
-  subsets: ['latin'],
-  weight: ['400', '500', '700'],
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
 });
 const quicksand = Quicksand({
-  subsets: ['latin'],
-  variable: '--font-quicksand',
+  subsets: ["latin"],
+  variable: "--font-quicksand",
 });
 
 // Extended metadata for SEO
 export const metadata: Metadata = {
-  title: 'Ziplyt – Book Trusted Local Workers Easily',
+  title: "Ziplyt – Book Trusted Local Workers Easily",
   description:
     "Ziplyt, Indias's first online home service app that let you book verified skilled local workers in no time, say goodbye to waiting , welcom instant solution.",
   keywords: [
-    'Ziplyt',
-    'book local workers',
-    'hire electrician online',
-    'trusted home services',
-    'plumber near me',
-    'local cleaning service',
-    'UrbanClap alternative',
-    'home service booking',
-    'verified professionals',
+    "Ziplyt",
+    "book local workers",
+    "hire electrician online",
+    "trusted home services",
+    "plumber near me",
+    "local cleaning service",
+    "UrbanClap alternative",
+    "home service booking",
+    "verified professionals",
   ],
-  authors: [{ name: 'Ziplyt Team', url: 'https://ziplyt.com' }],
+  authors: [{ name: "Ziplyt Team", url: "https://ziplyt.com" }],
   openGraph: {
-    title: 'Ziplyt – Trusted Local Worker Booking App',
+    title: "Ziplyt – Trusted Local Worker Booking App",
     description:
-      'Book electricians, plumbers, and more with Ziplyt. Instant, secure, and trusted services at your fingertips.',
-    url: 'https://ziplyt.com',
-    siteName: 'Ziplyt',
+      "Book electricians, plumbers, and more with Ziplyt. Instant, secure, and trusted services at your fingertips.",
+    url: "https://ziplyt.com",
+    siteName: "Ziplyt",
     images: [
       {
-        url: 'https://via.placeholder.com/1200x630.png?text=Ziplyt+Preview', // placeholder image
+        url: "/workerurlimage.png",
         width: 1200,
         height: 630,
-        alt: 'Ziplyt App Preview Placeholder Image',
+        alt: "Ziplyt App Preview Placeholder Image",
       },
     ],
-    type: 'website',
+    type: "website",
+  },
+  icons: {
+    icon: "/favicon.png",
   },
 };
 
@@ -76,14 +79,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body
         className={`${openSans.variable} ${inter.variable} ${roboto.variable} ${poppins.variable} ${quicksand.variable} antialiased`}
       >
         {/* <Navbar /> */}
         {children}
         <Footer />
-        <GoogleAnalytics gaId='G-HBQZ9SX1KW' />
+        <GoogleAnalytics gaId="G-HBQZ9SX1KW" />
         <Toaster />
         {/* <SpeedInsights /> */}
       </body>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,52 +1,49 @@
-import Image from 'next/image';
-import React from 'react';
-import Link from 'next/link';
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className='bg-deep py-4 mt-14'>
-      <div className='w-full px-8'>
+    <footer className="bg-deep py-4 mt-14">
+      <div className="w-full px-8">
         {/* LOGO + TAGLINE */}
-        <h2 className='w-full text-[42px] text-light/80 font-semibold font-main text-center'>
+        <h2 className="w-full text-[42px] text-light/80 font-semibold font-main text-center">
           Ziplyt
         </h2>
-        <p className='text-center text-[24px] font-poppins text-light/80 mt-3'>
+        <p className="text-center text-[24px] font-poppins text-light/80 mt-3">
           Zip it , Fix it , Love it
         </p>
-        <hr className='w-[90%] mx-auto border-b-[.1px] border-light/40 my-2' />
+        <hr className="w-[90%] mx-auto border-b-[.1px] border-light/40 my-2" />
 
-        <div className='w-full px-4'>
-          <div className='max-w-7xl mx-auto grid grid-cols-1 min-[900px]:grid-cols-3 w-full mt-6 gap-10'>
+        <div className="w-full px-4">
+          <div className="max-w-7xl mx-auto grid grid-cols-1 min-[900px]:grid-cols-3 w-full mt-6 gap-10">
             {/* NEWSLETTER + SOCIAL LINKS */}
-            <div className='flex flex-col items-center text-center px-4'>
-              <p className='font-inter font-light text-[15px] text-white tracking-tight leading-snug text-balance mb-2'>
-                Stay Updated with every offer and news. By clicking{' '}
-                <span className='font-semibold tracking-wide'>Subscribe</span>{' '}
-                you agree to our{' '}
-                <span className='underline underline-offset-2 font-extralight'>
+            <div className="flex flex-col items-center text-center px-4">
+              <p className="font-inter font-light text-[15px] text-white tracking-tight leading-snug text-balance mb-2">
+                Stay Updated with every offer and news. By clicking{" "}
+                <span className="font-semibold tracking-wide">Subscribe</span>{" "}
+                you agree to our{" "}
+                <span className="underline underline-offset-2 font-extralight">
                   terms and conditions.
                 </span>
               </p>
               {/* EMAIL AND SUBSCRIBE */}
-              <div className='mt-4 mb-2 w-full flex justify-center'>
-                <form className='w-full max-w-sm'>
-                  <label
-                    htmlFor='email'
-                    className='sr-only'
-                  >
+              <div className="mt-4 mb-2 w-full flex justify-center">
+                <form className="w-full max-w-sm">
+                  <label htmlFor="email" className="sr-only">
                     Email
                   </label>
-                  <div className='flex items-center bg-gray-300 rounded-full px-2 py-1'>
+                  <div className="flex items-center bg-gray-300 rounded-full px-2 py-1">
                     <input
-                      id='email'
-                      type='email'
-                      placeholder='Enter your email'
-                      className='flex-grow bg-transparent text-black text-sm px-3 py-2 outline-none'
+                      id="email"
+                      type="email"
+                      placeholder="Enter your email"
+                      className="flex-grow bg-transparent text-black text-sm px-3 py-2 outline-none"
                       required
                     />
                     <button
-                      type='submit'
-                      className='bg-primary-blue text-white text-sm px-4 py-2 rounded-full hover:bg-gray-800 transition'
+                      type="submit"
+                      className="bg-primary-blue text-white text-sm px-4 py-2 rounded-full hover:bg-gray-800 transition"
                     >
                       Subscribe
                     </button>
@@ -54,39 +51,39 @@ const Footer = () => {
                 </form>
               </div>
               {/* SOCILA LINKS */}
-              <div className='flex gap-4 justify-center mt-5'>
+              <div className="flex gap-4 justify-center mt-5">
                 <Link
-                  href='https://www.instagram.com'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://www.instagram.com/ziplyt.official?utm_source=qr&igsh=emRmaXA4M2Z1bnV3"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   <Image
-                    src='/instagram.png'
-                    alt='Instagram'
+                    src="/instagram.png"
+                    alt="Instagram"
                     width={28}
                     height={28}
                   />
                 </Link>
                 <Link
-                  href='https://www.linkedin.com'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://www.linkedin.com/company/ziplyt/?viewAsMember=true"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   <Image
-                    src='/linkedin.png'
-                    alt='LinkedIn'
+                    src="/linkedin.png"
+                    alt="LinkedIn"
                     width={28}
                     height={28}
                   />
                 </Link>
                 <Link
-                  href='https://www.facebook.com'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://www.facebook.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   <Image
-                    src='/facebook.png'
-                    alt='Facebook'
+                    src="/facebook.png"
+                    alt="Facebook"
                     width={28}
                     height={28}
                   />
@@ -95,21 +92,21 @@ const Footer = () => {
             </div>
 
             {/* COMPANY LINKS */}
-            <div className='px-4 flex flex-col items-center text-center'>
-              <h2 className='text-lg font-semibold mb-3 font-inter text-white'>
+            <div className="px-4 flex flex-col items-center text-center">
+              <h2 className="text-lg font-semibold mb-3 font-inter text-white">
                 Company
               </h2>
-              <ul className='space-y-3 text-[18px] text-gray-300'>
+              <ul className="space-y-3 text-[18px] text-gray-300">
                 <li>
-                  <Link href='/#'>
-                    <span className='hover:text-white transition cursor-pointer'>
+                  <Link href="/#">
+                    <span className="hover:text-white transition cursor-pointer">
                       About Us
                     </span>
                   </Link>
                 </li>
                 <li>
-                  <Link href='/reachus'>
-                    <span className='hover:text-white transition cursor-pointer'>
+                  <Link href="/reachus">
+                    <span className="hover:text-white transition cursor-pointer">
                       Reach Us
                     </span>
                   </Link>
@@ -118,36 +115,33 @@ const Footer = () => {
             </div>
 
             {/* SUPPORT */}
-            <div className='px-4 flex flex-col items-center text-center'>
-              <h2 className='text-lg font-semibold mb-3 font-inter text-white text-left'>
+            <div className="px-4 flex flex-col items-center text-center">
+              <h2 className="text-lg font-semibold mb-3 font-inter text-white text-left">
                 Support
               </h2>
-              <ul className='text-lg text-gray-300 flex flex-col gap-3'>
-                <li className='flex flex-col sm:flex-row gap-1'>
-                  <span className='block text-gray-400'>Email:</span>
-                  <a
-                    href='mailto:hello@ziplyt.com'
-                    className='text-white'
-                  >
+              <ul className="text-lg text-gray-300 flex flex-col gap-3">
+                <li className="flex flex-col sm:flex-row gap-1">
+                  <span className="block text-gray-400">Email:</span>
+                  <a href="mailto:hello@ziplyt.com" className="text-white">
                     hello@ziplyt.com
                   </a>
                 </li>
-                <li className='flex flex-col sm:flex-row gap-1'>
-                  <span className='block text-gray-400'>Phone:</span>
-                  <span className='text-white'>+91 62806 20947</span>
+                <li className="flex flex-col sm:flex-row gap-1">
+                  <span className="block text-gray-400">Phone:</span>
+                  <span className="text-white">+91 62806 20947</span>
                 </li>
-                <li className='flex flex-col sm:flex-row gap-1'>
-                  <span className='block text-gray-400'>Location:</span>
-                  <span className='text-white'>Ludhiana, India</span>
+                <li className="flex flex-col sm:flex-row gap-1">
+                  <span className="block text-gray-400">Location:</span>
+                  <span className="text-white">Ludhiana, India</span>
                 </li>
               </ul>
             </div>
           </div>
         </div>
       </div>
-      <hr className='w-[90%] mx-auto border-b-[.1px] border-light/40 my-5' />
+      <hr className="w-[90%] mx-auto border-b-[.1px] border-light/40 my-5" />
       {/* Bottom Bar */}
-      <div className='mt-5 text-center text-[16px] text-gray-200'>
+      <div className="mt-5 text-center text-[16px] text-gray-200">
         © {new Date().getFullYear()} Ziplyt. All rights reserved.
       </div>
     </footer>
